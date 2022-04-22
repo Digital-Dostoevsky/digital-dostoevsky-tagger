2dd46a96b5a5aa43c1222077538014f1b15615be
Simon Wiles <simonjwiles@gmail.com>
2022-04-22 14:37:33 -0700

    Use `re.VERBOSE` regular expressions for complicated ones

--- direct_speech.py
+++ direct_speech.py
@@ -3,7 +3,18 @@ import re
 
 def markup_direct_speech(text):
 
-    text = re.sub(r"(?<!» )— ([^—\n]+)(\s*)", r"<said>\g<1></said>\g<2>", text)
+    text = re.sub(
+        r"""
+            (?<!»\s)        # ignore emdashes which follow chevrons
+            —\s             # the emdash and whitespace (normalizing to a single space)
+            ([^—\n]+)       # everything up to another emdash or the end of the line
+            (\s*)           # any trainling whitespace which may precede a second emdash
+        """,
+        r"— <said>\g<1></said>\g<2>",
+        text,
+        flags=re.VERBOSE,
+    )
+
     # replace bounding chevrons with <said> tags
     text = re.sub(r"«[^»]+»", r"<said>\g<0></said>", text)
 
