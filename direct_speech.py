44d2ab9117bffeb8a98354bf1998dc77c071b01f
Simon Wiles <simonjwiles@gmail.com>
2022-04-22 14:37:06 -0700

    Add a comment

--- direct_speech.py
+++ direct_speech.py
@@ -4,6 +4,7 @@ import re
 def markup_direct_speech(text):
 
     text = re.sub(r"(?<!» )— ([^—\n]+)(\s*)", r"<said>\g<1></said>\g<2>", text)
+    # replace bounding chevrons with <said> tags
     text = re.sub(r"«[^»]+»", r"<said>\g<0></said>", text)
 
     # move trainling commas and whitespace outside of utterances
