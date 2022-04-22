23272cf8f7ba96ed0ff0bb785a37919507ceeb08
Simon Wiles <simonjwiles@gmail.com>
2022-04-22 14:36:42 -0700

    Add a quick feedback mechanism for exploring direct speech markup

--- direct_speech.py
+++ direct_speech.py
@@ -10,3 +10,17 @@ def markup_direct_speech(text):
     text = re.sub(r"([\s,]+)</said>", r"</said>\g<1>", text)
 
     return text
+
+
+if __name__ == "__main__":
+    import sys
+
+    with open(sys.argv[1], "rt") as _fh:
+        text = _fh.read()
+
+    for line in text.splitlines():
+        markedup_line = markup_direct_speech(line)
+        if line != markedup_line:
+            print(line)
+            print(markup_direct_speech(line))
+            print()
