f575d233a27e2632bc3da566e9cf2d2b478b5ad3
Simon Wiles <simonjwiles@gmail.com>
2022-04-26 21:53:30 -0700

    Tidy up unused code

--- direct_speech.py
+++ direct_speech.py
@@ -52,12 +52,10 @@ def markup_direct_speech(text: str) -> str:
     # replace bounding guillemets with <said> tags
     text = re.sub(r"«[^»\n]+»", rf"{tag_open}\g<0>{tag_close}", text)
 
+    # replace temporary markers with <said> tags
     text = text.replace(
         tag_open, "<said {}>".format(" ".join(f'{k}=""' for k in ATTRIBS))
     )
-
-    # replace temporary markers with <said> tags
-    text = text.replace(tag_open, "<said>")
     text = text.replace(tag_close, "</said>")
     return text
 
