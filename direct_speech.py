f08d5ecd15b4ca2675e2a3f3827e7abedb00233c
Simon Wiles <simonjwiles@gmail.com>
2022-04-28 18:07:09 -0700

    Parse direct speech line-by-line

--- direct_speech.py
+++ direct_speech.py
@@ -16,48 +16,54 @@ def markup_direct_speech(text: str) -> str:
     tag_open = "\uFFFE"
     tag_close = "\uFFFF"
 
-    # first pass for direct speech offset by an emdash
-    text = re.sub(
-        rf"""
-          (?<![,{p}]\s)       # discount emdashes which follow terminating punctuation
-          —\s+                # the emdash and any whitespace
-          (.+?)               # lazily capture everything up to...
-          (?=[,{p}]\s—|\n|$)  # emdash preceded by a comma, terminating punct., or EOL
-          ([{p}])?            # capture trailing terminating punctuation for inclusion
-        """,
-        # any whitespace folling the emdash is normalized to a single space
-        rf"— {tag_open}\g<1>\g<2>{tag_close}",
-        text,
-        flags=re.VERBOSE,
-    )
+    text_out = []
 
-    # second pass for utterances that resume after an inquit that terminates with a
-    #  comma or a period
-    text = re.sub(
-        rf"""
-          (?<!{tag_close})    # discount anything already marked for tagging
-          ([,.])\s*           # capture a comma or period that terminates an inquit
-          —\s+                # the emdash and any whitespace
-          (?!{tag_open})      # discount emdashes which follow terminating punctuation
-          (.+?)               # lazily capture everything up to...
-          (?=[,{p}]\s—|\n|$)  # emdash preceded by a comma, terminating punct., or EOL
-          ([{p}])?            # capture trailing terminating punctuation for inclusion
-        """,
-        # any whitespace following the comma and emdash are normalized to a single space
-        rf"\g<1> — {tag_open}\g<2>\g<3>{tag_close}",
-        text,
-        flags=re.VERBOSE,
-    )
+    for line in text.splitlines():
+        # first pass for direct speech offset by an emdash
+        line = re.sub(
+            rf"""
+            (?<![,{p}]\s)       # discount emdashes which follow terminating punctuation
+            —\s+                # the emdash and any whitespace
+            (.+?)               # lazily capture everything up to...
+            (?=[,{p}]\s—|\n|$)  # emdash preceded by a comma, terminating punct., or EOL
+            ([{p}])?            # capture trailing terminating punctuation for inclusion
+            """,
+            # any whitespace folling the emdash is normalized to a single space
+            rf"— {tag_open}\g<1>\g<2>{tag_close}",
+            line,
+            flags=re.VERBOSE,
+        )
 
-    # replace bounding guillemets with <said> tags
-    text = re.sub(r"«[^»\n]+»", rf"{tag_open}\g<0>{tag_close}", text)
+        # second pass for utterances that resume after an inquit that terminates with a
+        #  comma or a period
+        line = re.sub(
+            rf"""
+            (?<!{tag_close})    # discount anything already marked for tagging
+            ([,.])\s*           # capture a comma or period that terminates an inquit
+            —\s+                # the emdash and any whitespace
+            (?!{tag_open})      # discount emdashes which follow terminating punctuation
+            (.+?)               # lazily capture everything up to...
+            (?=[,{p}]\s—|\n|$)  # emdash preceded by a comma, terminating punct., or EOL
+            ([{p}])?            # capture trailing terminating punctuation for inclusion
+            """,
+            # any whitespace following the comma and emdash are normalized to a single space
+            rf"\g<1> — {tag_open}\g<2>\g<3>{tag_close}",
+            line,
+            flags=re.VERBOSE,
+        )
 
-    # replace temporary markers with <said> tags
-    text = text.replace(
-        tag_open, "<said {}>".format(" ".join(f'{k}=""' for k in ATTRIBS))
-    )
-    text = text.replace(tag_close, "</said>")
-    return text
+        # replace bounding guillemets with <said> tags
+        line = re.sub(r"«[^»\n]+»", rf"{tag_open}\g<0>{tag_close}", line)
+
+        # replace temporary markers with <said> tags
+        line = line.replace(
+            tag_open, "<said {}>".format(" ".join(f'{k}=""' for k in ATTRIBS))
+        )
+        line = line.replace(tag_close, "</said>")
+
+        text_out.append(line)
+
+    return "\n".join(text_out)
 
 
 if __name__ == "__main__":
