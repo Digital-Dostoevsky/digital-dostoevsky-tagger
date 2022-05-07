5e29d56c3154efeb12cad2185042d5666b8f87fa
Simon Wiles <simonjwiles@gmail.com>
2022-05-06 17:10:59 -0700

    Complete type hints

--- proper_names.py
+++ proper_names.py
@@ -1,7 +1,7 @@
 import re
 
 
-def markup_proper_names(text, names, tag):
+def markup_proper_names(text: str, names: list[str], tag: str) -> str:
 
     re_proper_names = re.compile(
         rf"\b(?:{'|'.join(sorted(names, key=len, reverse=True))})\b"
