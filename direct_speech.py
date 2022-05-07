34fa1582092deda5e286b465c7bb936998cc962c
Simon Wiles <simonjwiles@gmail.com>
2022-05-06 17:03:09 -0700

    Rewrite regex parsing code to cope with more complicated examples
    Note this required the `regex` library from PyPI rather than the `re` library from the stdlib.

--- direct_speech.py
+++ direct_speech.py
@@ -1,4 +1,4 @@
-import re
+import regex as re
 
 ATTRIBS = ["aloud", "direct", "who", "toWhom"]
 
@@ -13,55 +13,107 @@ def markup_direct_speech(text: str) -> str:
     p = "?!).»"
 
     # designate two unicode "noncharacters" for use as temporary markers
-    tag_open = "\uFFFE"
-    tag_close = "\uFFFF"
+    start = "\uFFFE"
+    end = "\uFFFF"
 
     text_out = []
-
     for line in text.splitlines():
+
+        # mark text inside bounding straight double quotes
+        line = re.sub(r'«?"[^"\n]+"', rf"{start}\g<0>{end}", line)
+
+        line_out = line
+
         # first pass for direct speech offset by an emdash
-        line = re.sub(
+        line_out = re.sub(
             rf"""
-            (?<![,{p}]\s)       # discount emdashes which follow terminating punctuation
-            —\s+                # the emdash and any whitespace
-            (.+?)               # lazily capture everything up to...
-            (?=[,{p}]\s—|\n|$)  # emdash preceded by a comma, terminating punct., or EOL
-            ([{p}])?            # capture trailing terminating punctuation for inclusion
+            (?<!{start})          # onset is not already marked
+            (?<!{end}\s)          # or follows immediately from a marked sequence
+
+            (?:(?:                # emdash-offset utterance
+                                  # -----------------------
+
+              (?<![,{p}\w]\s)     # emdash does not follow terminating punctuation
+              (—\s+)              # capture the emdash and any whitespace
+              (.+?)               # lazily capture everything up to...
+              (?=[,{p}]\s—|\n|$)  # terminating punctuation, a new emdash, or end of line
+
+            ) | (?:               # guillemet-offset utterance
+                                  # --------------------------
+
+              («)                 # capture the guillemet
+              ([^«]+?)            # lazily capture everything up to...
+              (?=
+                                  # an emdash if it's preceded by terminating punctuation
+                                  #  that's not itself preceded by a marked sequence
+                (?<!"|{end})[,{p}]\s—
+                |»(?!{end})       # or a guillemet (unless it closes a marked sequence)
+                |\n|$             # or end of line
+              )
+            ))
+            ([{p}])?              # capture any trailing terminating punctuation
             """,
-            # any whitespace folling the emdash is normalized to a single space
-            rf"— {tag_open}\g<1>\g<2>{tag_close}",
-            line,
+            lambda m: "".join(
+                [
+                    (m.group(1).strip() + " " if m.group(1) else m.group(3)),
+                    start,
+                    (m.group(2) or m.group(4)),
+                    m.group(5) or "",
+                    end,
+                ]
+            ),
+            line_out,
             flags=re.VERBOSE,
         )
 
-        # second pass for utterances that resume after an inquit that terminates with a
-        #  comma or a period
-        line = re.sub(
-            rf"""
-            (?<!{tag_close})    # discount anything already marked for tagging
-            ([,.])\s*           # capture a comma or period that terminates an inquit
-            —\s+                # the emdash and any whitespace
-            (?!{tag_open})      # discount emdashes which follow terminating punctuation
-            (.+?)               # lazily capture everything up to...
-            (?=[,{p}]\s—|\n|$)  # emdash preceded by a comma, terminating punct., or EOL
-            ([{p}])?            # capture trailing terminating punctuation for inclusion
-            """,
-            # any whitespace following the comma and emdash are normalized to a single space
-            rf"\g<1> — {tag_open}\g<2>\g<3>{tag_close}",
-            line,
-            flags=re.VERBOSE,
-        )
+        # second pass for utterances offset with emdash and that resume after
+        #  an inquit that terminates with a comma or a period
+        if (
+            # if there's already a marked utterance in this line
+            f"— {start}" in line_out
+            or re.search(rf"«{start}[^»]+{end}", line_out)
+            or re.match(rf"«{start}[^»]+»{end}", line_out)
+        ):
+            line_out = re.sub(
+                rf"""
+                                  # onset follows a marked utterance earlier in the line
+                (?<={end}[^{end}]+?) 
+                (?<!{end})        # onset does not immediately follow a marked utterance
+
+                ([,.])\s*         # capture a comma or period that terminates an inquit
+                —\s+              # match the emdash and any whitespace
+                (?!{start})       # unless the utterance is already marked
+                (.+?)             # lazily capture everything up to...
+                (?=
+                  [,{p}]\s—       # an emdash preceded by terminating punctuation
+                  |»(?!{end})     # or a guillemet (unless it closes a marked sequence)
+                  |\n|$           # or end of line
+                )
+                ([{p}]+)?         # capture any trailing terminating punctuation
+                """,
+                rf"\g<1> — {start}\g<2>\g<3>{end}",
+                line_out,
+                flags=re.VERBOSE,
+            )
+
+        # post-hoc
+        line_out = line_out.replace(f"«{start}", f"{start}«")
+        line_out = line_out.replace(f"».{end}", f"»{end}.")
 
         # replace bounding guillemets with <said> tags
-        line = re.sub(r"«[^»\n]+»", rf"{tag_open}\g<0>{tag_close}", line)
+        line_out = re.sub(
+            rf"(?<!{start})«.+?(»(?!{end})|\n|$)",
+            rf"{start}\g<0>{end}",
+            line_out,
+        )
 
         # replace temporary markers with <said> tags
-        line = line.replace(
-            tag_open, "<said {}>".format(" ".join(f'{k}=""' for k in ATTRIBS))
+        line_out = line_out.replace(
+            start, "<said {}>".format(" ".join(f'{k}=""' for k in ATTRIBS))
         )
-        line = line.replace(tag_close, "</said>")
+        line_out = line_out.replace(end, "</said>")
 
-        text_out.append(line)
+        text_out.append(line_out)
 
     return "\n".join(text_out)
 
@@ -70,12 +122,12 @@ if __name__ == "__main__":
     # The following code is used for testing and development purposes only.
     import sys
 
-    with open(sys.argv[1], "rt") as _fh:
+    with open(sys.argv[1], "rt", encoding="utf8") as _fh:
         text = _fh.read()
 
     for line in text.splitlines():
-        markedup_line = markup_direct_speech(line)
-        if line != markedup_line:
+        marked_up_line = markup_direct_speech(line)
+        if line != marked_up_line:
             print(line)
-            print(markup_direct_speech(line))
+            print(marked_up_line)
             print()
